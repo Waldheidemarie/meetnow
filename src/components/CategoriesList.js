b4b3d6f3f7f2295dd6bd5eee54d9af4044fff9d9
Abubakar Usman <abusmoh@gmail.com>
2018-07-28 00:36:43 -0400

    added categories component

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFlask,
+        faBuilding,
+        faMusic,
+        faVolleyballBall,
+        faTshirt,
+        faLifeRing,
+        faPeopleCarry,
+        faBriefcaseMedical,
+        faWrench,
+        faSearch,
+        faList,
+        faUserFriends
+} from '@fortawesome/free-solid-svg-icons';
 
 const CategoriesList = () => {
     return (
@@ -6,18 +20,18 @@ const CategoriesList = () => {
             <h4>Categories ...</h4>
             <hr />
             <ul>
-                <li>Science & Tech</li>
-                <li>Business</li>
-                <li>Entertainment</li>
-                <li>Sports</li>
-                <li>Fashion</li>
-                <li>Lifestyle</li>
-                <li>Volunteering</li>
-                <li>Age Groups</li>
-                <li>Health</li>
-                <li>Career Fairs</li>
-                <li>Research Groups</li>
-                <li>Conferences</li>
+                <li><FontAwesomeIcon icon={faFlask}/> Science & Tech</li>
+                <li><FontAwesomeIcon icon={faBuilding}/> Business</li>
+                <li><FontAwesomeIcon icon={faMusic}/> Entertainment</li>
+                <li><FontAwesomeIcon icon={faVolleyballBall}/> Sports</li>
+                <li><FontAwesomeIcon icon={faTshirt}/> Fashion</li>
+                <li><FontAwesomeIcon icon={faLifeRing}/> Lifestyle</li>
+                <li><FontAwesomeIcon icon={faPeopleCarry}/> Volunteering</li>
+                <li><FontAwesomeIcon icon={faUserFriends}/> Age Groups</li>
+                <li><FontAwesomeIcon icon={faBriefcaseMedical}/> Health</li>
+                <li><FontAwesomeIcon icon={faWrench}/> Career Fairs</li>
+                <li><FontAwesomeIcon icon={faSearch}/> Research Groups</li>
+                <li><FontAwesomeIcon icon={faList}/> Conferences</li>
             </ul>
         </div>
     )
